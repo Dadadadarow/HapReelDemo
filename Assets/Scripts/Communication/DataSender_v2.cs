fe0b4f344c774b9785de8eef2bf467d0861c2ad0
entanguruguru <54872924+entanguruguru@users.noreply.github.com>
2023-12-04 17:56:23 +0900

    Add Communication Scripts

---
+++ Assets/Scripts/Communication/DataSender_v2.cs
@@ -0,0 +1,51 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class DataSender_v2 : MonoBehaviour
+{
+    public SerialHandler_v2 serialHandler;
+
+    [System.Serializable]
+    public struct MotorControlPacket
+    {       
+        public int   direction;  // 2つのモータで提示する力覚の方向を指示 0:STOP, 1:FORTH, 2:BACK, 3:LEFT, 4:RIGHT
+
+        public float returntime; // モータを戻す時間
+        public float M;          // モータの最大回転角度[度]（トルク量）
+        public float freq;       // モータの周波数(未使用)
+        public float A;          // 減衰振動の振幅（衝撃力の大きさ）
+        public float B;          // 減衰振動の減衰率（モータを戻す時間により変化）
+    }
+
+    public MotorControlPacket motorControlPacket;
+
+    int[] directions = new int[] {0, 1, 2, 3, 4}; // 2つのモータで提示する力覚の方向を指示 0:STOP, 1:FORTH, 2:BACK, 3:LEFT, 4:RIGHT
+    int clickCount = 0;
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (Input.GetKeyDown(KeyCode.Space))
+        {
+            SendMotorControlPacket(directions[clickCount], 0.2f, 10, 0.0f, 50f, 10.0f);
+            
+            if(clickCount < 4)  clickCount++;
+            else clickCount = 0;
+        }
+    }
+
+    public void SendMotorControlPacket(int direction, float returntime, float M, float freq, float A, float B)
+    {
+        motorControlPacket.direction = direction;
+        motorControlPacket.returntime = returntime;
+        motorControlPacket.M = M;
+        motorControlPacket.freq = freq;
+        motorControlPacket.A = A;
+        motorControlPacket.B = B;
+
+        string message = motorControlPacket.direction.ToString() + "," + motorControlPacket.returntime.ToString() + "," + motorControlPacket.M.ToString() + "," + motorControlPacket.freq.ToString() + "," + motorControlPacket.A.ToString() + "," + motorControlPacket.B.ToString();
+        serialHandler.SendData(message);
+        // Debug.Log(message);
+    }
+}
---
+++ Assets/Scripts/Communication/SerialHandler_v2.cs
@@ -0,0 +1,134 @@
+/*
+    DataReceived イベントを使用した実装、データを受け取ることができないのでポーリング方式に切り替え
+    受信処理は別スレッドにする
+*/
+
+using System.IO.Ports;
+using UnityEngine;
+using System.Threading;
+using System;
+
+public class SerialHandler_v2 : MonoBehaviour
+{
+    public string portName = "COM8";  // 適切なポート名を設定
+    public int baudRate = 115200;
+    private SerialPort serialPort;
+    private Thread thread;
+
+    public string readMessage;
+    private bool isRunning = false;
+
+    private void Start()
+    {
+        OpenConnection();
+        Thread.Sleep(2000);  // 2秒待機
+    }
+
+    private void OpenConnection()
+    {
+        try
+        {
+            if (serialPort != null && serialPort.IsOpen)
+            {
+                serialPort.Close();
+                Debug.Log("Port closed.");
+            }
+
+            serialPort = new SerialPort(portName, baudRate);
+            serialPort.Open();
+            // serialPort.ReadTimeout = 500;
+
+            thread = new Thread(Read);
+            thread.Start();
+
+            isRunning = true;
+            Debug.Log("Port opened.");
+        }
+        catch (System.Exception e)
+        {
+            Debug.LogWarning(e.Message);
+        }
+    }
+    // private void Update()
+    // {
+    //     // スペースキーを押すとデータを送信
+    //     // if (Input.GetKeyDown(KeyCode.Space))
+    //     // {
+    //         // 例として、時間に応じて変化する2つのfloat値を送信します
+    //         float value1 = Mathf.Sin(Time.time);
+    //         float value2 = Mathf.Cos(Time.time);
+
+    //         string message = value1.ToString("F4") + "," + value2.ToString("F4"); // カンマ区切りの文字列
+    //         SendData(message);
+    //     // }
+    // }
+
+    private void Read() 
+    {
+        while (isRunning) 
+        {
+            if (serialPort != null && serialPort.IsOpen) 
+            {
+                try 
+                {
+                    // if (serialPort.BytesToRead > 0)
+                    // {
+                    //     string readMessage = serialPort.ReadLine();
+                    //     Debug.Log("Received: " + readMessage);
+                    // }
+                    // Debug.Log("BytesToRead: " + serialPort.BytesToRead);
+                    while (serialPort.BytesToRead > 0)
+                    {
+                        string readMessage = serialPort.ReadLine();
+                        Debug.Log("Received: " + readMessage);
+                    }
+                }
+                catch (System.Exception e) 
+                {
+                    Debug.LogWarning(e.Message);
+                }
+            }
+            Thread.Sleep(10); // CPUを圧迫しないための小休止
+        }
+    }
+
+    public void SendData(string message)
+    {
+        if (serialPort != null && serialPort.IsOpen)
+        {
+            serialPort.WriteLine(message);
+            // Debug.Log("Sent: " + message);
+        }
+    }
+
+    private void OnApplicationQuit()
+    {
+        isRunning = false;
+        if (thread != null && thread.IsAlive) 
+        {
+            thread.Join();
+            Debug.Log("Thread closed.");
+        }
+
+        if (serialPort != null && serialPort.IsOpen) 
+        {
+            serialPort.Close();
+            serialPort.Dispose();
+            Debug.Log("Port closed.");
+        }
+    }
+    void OnDestroy()
+    {
+        isRunning = false;
+        if (thread != null && thread.IsAlive) 
+        {
+            thread.Join();
+        }
+
+        if (serialPort != null && serialPort.IsOpen) 
+        {
+            serialPort.Close();
+            serialPort.Dispose();
+        }
+    }
+}
