640702c580a5c1d0b664b4dc9e4ab43c8963991f
Dadadadarow <ground3101@gmail.com>
2023-12-08 16:17:58 +0900

    add concept mode draft1

---
+++ Assets/CollisionDetection4cq.cs
@@ -0,0 +1,85 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CollisionDetection4cq : MonoBehaviour
+{
+    public DataSender dataSender;
+    public TaskManager taskMode;
+    void OnCollisionEnter(Collision collision)
+    {
+        // Box Collider の表面上の中心点を取得
+        Vector3 center = GetComponent<BoxCollider>().bounds.center;
+        
+        // 衝突点を取得
+        Vector3 collisionPoint = collision.contacts[0].point;
+
+        // 衝突点と中心点の距離を計算
+        float distance = Vector3.Distance(center, collisionPoint);
+
+
+        // 例: 距離に応じて値を決定し、送信する
+        Vector3 localCollisionPoint = transform.InverseTransformPoint(collisionPoint);
+        // Debug.Log(localCollisionPoint.x);
+        // racket position >>> left |0|1|2|3|4| right <<<
+
+        if (taskMode.isTorque)
+        {
+            // 2つのモータで提示する力覚の方向を指示 0:STOP, 1:FORTH, 2:BACK, 3:LEFT, 4:RIGHT
+            if (-0.1f <= localCollisionPoint.x && localCollisionPoint.x < -0.06)
+            {
+                dataSender.SendMotorControlPacket(3,0.2f, 10, 0.0f, 50f, 10.0f);
+                Debug.Log("Detect 0");
+            }
+            else if (-0.06f <= localCollisionPoint.x && localCollisionPoint.x < -0.02f)
+            {
+                dataSender.SendMotorControlPacket(3, 0.2f, 10, 0.0f, 50f, 10.0f);
+                Debug.Log("Detect 1");
+            }
+            else if (-0.02f <= localCollisionPoint.x && localCollisionPoint.x < 0.02f)
+            {
+                dataSender.SendMotorControlPacket(2, 0.2f, 10, 0.0f, 50f, 10.0f);
+                Debug.Log("Detect 2");
+            }
+            else if (0.02f <= localCollisionPoint.x && localCollisionPoint.x < 0.06f)
+            {
+                dataSender.SendMotorControlPacket(4, 0.2f, 10, 0.0f, 50f, 10.0f);
+                Debug.Log("Detect 3");
+            }
+            else if (0.06f <= localCollisionPoint.x && localCollisionPoint.x <= 0.1f)
+            {
+                dataSender.SendMotorControlPacket(4, 0.2f, 10, 0.0f, 50f, 10.0f);
+                Debug.Log("Detect 4");
+            }
+        }
+        else
+        {
+            // 2つのモータで提示する力覚の方向を指示 0:STOP, 1:FORTH, 2:BACK, 3:LEFT, 4:RIGHT
+            if (-0.1f <= localCollisionPoint.x && localCollisionPoint.x < -0.06)
+            {
+                dataSender.SendMotorControlPacket(0,0.2f, 10, 0.0f, 50f, 10.0f);
+                Debug.Log("Detect 0");
+            }
+            else if (-0.06f <= localCollisionPoint.x && localCollisionPoint.x < -0.02f)
+            {
+                dataSender.SendMotorControlPacket(0, 0.2f, 10, 0.0f, 50f, 10.0f);
+                Debug.Log("Detect 1");
+            }
+            else if (-0.02f <= localCollisionPoint.x && localCollisionPoint.x < 0.02f)
+            {
+                dataSender.SendMotorControlPacket(0, 0.2f, 10, 0.0f, 50f, 10.0f);
+                Debug.Log("Detect 2");
+            }
+            else if (0.02f <= localCollisionPoint.x && localCollisionPoint.x < 0.06f)
+            {
+                dataSender.SendMotorControlPacket(0, 0.2f, 10, 0.0f, 50f, 10.0f);
+                Debug.Log("Detect 3");
+            }
+            else if (0.06f <= localCollisionPoint.x && localCollisionPoint.x <= 0.1f)
+            {
+                dataSender.SendMotorControlPacket(0, 0.2f, 10, 0.0f, 50f, 10.0f);
+                Debug.Log("Detect 4");
+            }
+        }
+    }
+}
--- Assets/TaskManager.cs
+++ Assets/TaskManager.cs
@@ -6,10 +6,13 @@ public class TaskManager : MonoBehaviour
 {
     public GameObject ball;
     public GameObject racket;
-    public Transform respawnRight;
-    public Transform respawnLeft;
-    public Transform respawnMid;
-    //aaaa
+    public Transform respawnLL;
+    public Transform respawnL;
+    public Transform respawnM;
+    public Transform respawnR;
+    public Transform respawnRR;
+    public bool isTorque = false;
+
     // Start is called before the first frame update
     void Start()
     {
@@ -19,20 +22,68 @@ public class TaskManager : MonoBehaviour
     // Update is called once per frame
     void Update()
     {
-        if (Input.GetKeyDown(KeyCode.A))
-        {
-            racket.transform.rotation = Quaternion.Euler(0f, 0f, 0f);
-            Instantiate(ball, respawnLeft.position, Quaternion.identity);
-        }
+        // Sキーを押すとConceptモードを開始
         if (Input.GetKeyDown(KeyCode.S))
         {
-            racket.transform.rotation = Quaternion.Euler(0f, 0f, 0f);
-            Instantiate(ball, respawnMid.position, Quaternion.identity);
+            // 2秒ごとにランダムな位置にボールを生成する処理を開始し、6回繰り返す
+            InvokeRepeating("SpawnRandomBall", 0f, 2f);
+            Invoke("ChangeFeedbackMode", 10f); // 6秒後にモードを変更
+            Invoke("StopSpawning", 20f); // 12秒後に処理を停止
         }
-        if (Input.GetKeyDown(KeyCode.D))
+        if (Input.GetKeyDown(KeyCode.Escape))
         {
-            racket.transform.rotation = Quaternion.Euler(0f, 0f, 0f);
-            Instantiate(ball, respawnRight.position, Quaternion.identity);
+            StopSpawning();
+        }
+    }
+
+    // ランダムな位置にボールを生成するメソッド
+    void SpawnRandomBall()
+    {
+        Vector3 randomRespawnPosition = GetRandomRespawnPosition();
+        SpawnBall(randomRespawnPosition);
+    }
+
+    // ボールを生成して、5秒後に破棄するメソッド
+    void SpawnBall(Vector3 position)
+    {
+        racket.transform.rotation = Quaternion.Euler(0f, 0f, 0f);
+        GameObject newBall = Instantiate(ball, position, Quaternion.identity);
+        Destroy(newBall, 5f);
+    }
+
+    // ランダムな位置を取得するメソッド
+    Vector3 GetRandomRespawnPosition()
+    {
+        int randomIndex = Random.Range(0, 4);
+
+        switch (randomIndex)
+        {
+            case 0:
+                return respawnL.position;
+            case 1:
+                return respawnLL.position;
+            case 2:
+                return respawnM.position;
+            case 3:
+                return respawnR.position;
+            case 4:
+                return respawnRR.position;
+            default:
+                return respawnL.position;
+        }
+    }
+
+    // 処理を停止するメソッド
+    void StopSpawning()
+    {
+        CancelInvoke("SpawnRandomBall");
+    }
+
+    void ChangeFeedbackMode()
+    {
+        if (!isTorque)
+        {
+            isTorque = true;
         }
     }
 }
