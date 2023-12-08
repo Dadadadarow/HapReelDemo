7c90ce4baab9d4ba7542f43a6b614475ddb27189
Dadadadarow <ground3101@gmail.com>
2023-12-08 11:26:39 +0900

    test

--- Assets/TaskManager.cs
+++ Assets/TaskManager.cs
@@ -9,6 +9,7 @@ public class TaskManager : MonoBehaviour
     public Transform respawnRight;
     public Transform respawnLeft;
     public Transform respawnMid;
+    //aaaa
     // Start is called before the first frame update
     void Start()
     {
