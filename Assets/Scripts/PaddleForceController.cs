829460720cb5bb34350cf4a7da14b552838ae33b
Dadadadarow <ground3101@gmail.com>
2023-12-14 16:29:13 +0900

    racket system mod

--- Assets/Scripts/PaddleForceController.cs
+++ Assets/Scripts/PaddleForceController.cs
@@ -1,39 +1,3 @@
-// using System.Collections;
-// using System.Collections.Generic;
-// using UnityEngine;
-// using UnityEngine.InputSystem;
-// using Valve.VR.InteractionSystem;
-
-// public class PaddleForceController : MonoBehaviour
-// {
-//     public VelocityEstimator ve;
-//     [SerializeField] private float forceMultiplier = 0.03f;
-//     [SerializeField] private float rotationMultiplier = 10000f;
-//     private Rigidbody rb;
-
-//     private void Start()
-//     {
-//         rb = GetComponent<Rigidbody>();
-//     }
-
-//     private void OnCollisionEnter(Collision collision)
-//     {
-//         if (collision.gameObject.CompareTag("Ball"))
-//         {
-//             Rigidbody ballRb = collision.gameObject.GetComponent<Rigidbody>();
-//             Vector3 hitDirection = ve.GetVelocityEstimate();
-
-//             //normal vector of collision
-//             Vector3 collisionNormal = collision.contacts[0].normal;
-//             //calculate the rotation force depending on the racket speed and normal vector of collision
-//             Vector3 rotationForce = Vector3.Cross(hitDirection, collisionNormal) * rotationMultiplier;
-
-//             Debug.Log(rotationForce);
-//             ballRb.AddForce(hitDirection * forceMultiplier, ForceMode.Impulse);
-//             ballRb.AddTorque(rotationForce, ForceMode.Impulse);
-//         }
-//     }
-// }
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
@@ -43,10 +7,8 @@ using Valve.VR.InteractionSystem;
 public class PaddleForceController : MonoBehaviour
 {
     public VelocityEstimator ve;
-    [SerializeField] private float forceMultiplier = 0.03f;
+    [SerializeField] private float forceMultiplier = 0.2f;
     [SerializeField] private float rotationMultiplier = 10000f;
-    [SerializeField] private float velocityContribution = 0.2f; // 速度方向の寄与の割合
-    [SerializeField] private Vector3 forwardDirection = Vector3.forward; // 指定した正面方向
     private Rigidbody rb;
 
     private void Start()
@@ -59,23 +21,63 @@ public class PaddleForceController : MonoBehaviour
         if (collision.gameObject.CompareTag("Ball"))
         {
             Rigidbody ballRb = collision.gameObject.GetComponent<Rigidbody>();
-            Vector3 hitDirection = ve.GetVelocityEstimate().normalized;
+            float velocityMultiplier = ve.GetVelocityEstimate().magnitude;
+            // Vector3 hitDirection = ve.GetVelocityEstimate();
+            Vector3 hitDirection = new Vector3(1, 0, 0);
 
-            // 衝突した面の法線ベクトル
+            //normal vector of collision
             Vector3 collisionNormal = collision.contacts[0].normal;
+            //calculate the rotation force depending on the racket speed and normal vector of collision
+            // Vector3 rotationForce = Vector3.Cross(hitDirection, collisionNormal) * rotationMultiplier;
+            Vector3 rotationForce = hitDirection * rotationMultiplier*velocityMultiplier;
 
-            // 速度方向と法線ベクトルの合成ベクトルを求める
-            Vector3 combinedDirection = (hitDirection * (1 - velocityContribution) + collisionNormal * velocityContribution).normalized;
-
-            // 合成ベクトルに対して直線的な力を加える
-            ballRb.AddForce(combinedDirection * forceMultiplier, ForceMode.Impulse);
-
-            // 速度方向の寄与を抑えつつ回転力を計算
-            Vector3 rotationForce = Vector3.Cross(forwardDirection, combinedDirection) * rotationMultiplier;
-
-            Debug.Log(rotationForce);
+            // Debug.Log(rotationForce);
+            ballRb.AddForce(hitDirection * forceMultiplier, ForceMode.Impulse);
             ballRb.AddTorque(rotationForce, ForceMode.Impulse);
         }
     }
 }
+// using System.Collections;
+// using System.Collections.Generic;
+// using UnityEngine;
+// using UnityEngine.InputSystem;
+// using Valve.VR.InteractionSystem;
 
+// public class PaddleForceController : MonoBehaviour
+// {
+//     public VelocityEstimator ve;
+//     [SerializeField] private float forceMultiplier = 0.03f;
+//     [SerializeField] private float rotationMultiplier = 10000f;
+//     [SerializeField] private float velocityContribution = 0.2f; // 速度方向の寄与の割合
+//     [SerializeField] private Vector3 forwardDirection = Vector3.forward; // 指定した正面方向
+//     private Rigidbody rb;
+
+//     private void Start()
+//     {
+//         rb = GetComponent<Rigidbody>();
+//     }
+
+//     private void OnCollisionEnter(Collision collision)
+//     {
+//         if (collision.gameObject.CompareTag("Ball"))
+//         {
+//             Rigidbody ballRb = collision.gameObject.GetComponent<Rigidbody>();
+//             Vector3 hitDirection = ve.GetVelocityEstimate().normalized;
+
+//             // 衝突した面の法線ベクトル
+//             Vector3 collisionNormal = collision.contacts[0].normal;
+
+//             // 速度方向と法線ベクトルの合成ベクトルを求める
+//             Vector3 combinedDirection = (hitDirection * (1 - velocityContribution) + collisionNormal * velocityContribution).normalized;
+
+//             // 合成ベクトルに対して直線的な力を加える
+//             ballRb.AddForce(combinedDirection * forceMultiplier, ForceMode.Impulse);
+
+//             // 速度方向の寄与を抑えつつ回転力を計算
+//             Vector3 rotationForce = Vector3.Cross(forwardDirection, combinedDirection) * rotationMultiplier;
+
+//             Debug.Log(rotationForce);
+//             ballRb.AddTorque(rotationForce, ForceMode.Impulse);
+//         }
+//     }
+// }
--- Assets/Scripts/RespawnBalls.cs
+++ Assets/Scripts/RespawnBalls.cs
@@ -1,27 +1,80 @@
+// using System.Collections;
+// using System.Collections.Generic;
+// using UnityEngine;
+
+// public class RespawnBalls : MonoBehaviour
+// {
+//     public GameObject respawnPoint;
+
+//     public GameObject ball;
+//     [SerializeField] private float serve_speed = 0.007f;
+
+//     void Start()
+//     {
+        
+//     }
+
+//     // Update is called once per frame
+//     void Update()
+//     {
+//         if (Input.GetKeyDown(KeyCode.Space))
+//         {
+//             RespawnBall();
+//         }
+//     }
+//     private void RespawnBall()
+//     {
+//         Vector3 ballPosition = respawnPoint.transform.position;
+
+//         GameObject newBall = Instantiate(ball, ballPosition, transform.rotation);
+
+//         // サーブ方向をランダムに設定
+//         // float randomAngle = Random.Range(-2.0f, 2.0f); // -45度から45度のランダムな角度を生成
+//         float randomAngle = 90;
+//         // Vector3 serveDirection = Quaternion.Euler(0, randomAngle, 0);
+//         Vector3 serveDirection = Quaternion.Euler(0, randomAngle, 0) * Vector3.back;
+
+//         newBall.GetComponent<Rigidbody>().AddForce(serveDirection * serve_speed, ForceMode.Impulse);
+//         newBall.name = ball.name;
+
+//         Destroy(newBall, 10f);
+//     }
+// }
+
+
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.SceneManagement; // シーンマネジメントのために追加
 
 public class RespawnBalls : MonoBehaviour
 {
     public GameObject respawnPoint;
-
     public GameObject ball;
     [SerializeField] private float serve_speed = 0.007f;
+    [SerializeField] private float respawnInterval = 3f; // ボールが出る間隔
 
-    void Start()
-    {
-        
-    }
+    private float timeSinceLastRespawn = 0f;
+    private float totalTimeElapsed = 0f;
 
-    // Update is called once per frame
     void Update()
     {
-        if (Input.GetKeyDown(KeyCode.Space))
+        timeSinceLastRespawn += Time.deltaTime;
+        totalTimeElapsed += Time.deltaTime;
+
+        if (timeSinceLastRespawn >= respawnInterval)
         {
             RespawnBall();
+            timeSinceLastRespawn = 0f; // リセットして次のカウントを開始
+        }
+
+        // 60秒経過したらシーンを停止
+        if (totalTimeElapsed >= 60f)
+        {
+            StopScenePlayback();
         }
     }
+
     private void RespawnBall()
     {
         Vector3 ballPosition = respawnPoint.transform.position;
@@ -29,14 +82,23 @@ public class RespawnBalls : MonoBehaviour
         GameObject newBall = Instantiate(ball, ballPosition, transform.rotation);
 
         // サーブ方向をランダムに設定
-        // float randomAngle = Random.Range(-2.0f, 2.0f); // -45度から45度のランダムな角度を生成
         float randomAngle = 90;
-        // Vector3 serveDirection = Quaternion.Euler(0, randomAngle, 0);
         Vector3 serveDirection = Quaternion.Euler(0, randomAngle, 0) * Vector3.back;
 
         newBall.GetComponent<Rigidbody>().AddForce(serveDirection * serve_speed, ForceMode.Impulse);
         newBall.name = ball.name;
 
         Destroy(newBall, 10f);
+    }
+
+    // シーンの再生を停止するメソッド
+    private void StopScenePlayback()
+    {
+        // シーンの再生を停止するためのコード
+        // 例えば、以下のように現在のシーンを再読み込みすることでシーンを停止します。
+        // SceneManager.LoadScene(SceneManager.GetActiveScene().name);
+
+        // または、アプリケーション全体を終了させたい場合は以下のようにします。
+        Application.Quit();
     }
 }
